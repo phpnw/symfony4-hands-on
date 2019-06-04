69184e4be6e6a11c1a08726b10d9b76b647739cf
Jeremy Coates <jeremyc@magmadigital.co.uk>
2019-06-04 21:19:41 +0100

    Added a UserNormalizer class to normalise the user object.

---
+++ src/Normalizer/UserNormalizer.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Normalizer;
+
+
+use App\Entity\User;
+use Symfony\Component\Serializer\Exception\CircularReferenceException;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Exception\LogicException;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    /**
+     * Normalizes an object into a set of arrays/scalars.
+     *
+     * @param User $object Object to normalize
+     * @param string $format Format the normalization result will be encoded as
+     * @param array $context Context options for the normalizer
+     *
+     * @return array|string|int|float|bool
+     *
+     * @throws InvalidArgumentException   Occurs when the object given is not an attempted type for the normalizer
+     * @throws CircularReferenceException Occurs when the normalizer detects a circular reference when no circular
+     *                                    reference handler can fix it
+     * @throws LogicException             Occurs when the normalizer is not called in an expected context
+     * @throws ExceptionInterface         Occurs for all the other cases of errors
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        return [
+            'username' => $object->getUsername(),
+            'created_at' => $this->normalizer->normalize($object->getCreatedAt(), $format, $context),
+            'updated_at' => $this->normalizer->normalize($object->getUpdatedAt(), $format, $context),
+        ];
+    }
+
+    /**
+     * Checks whether the given class is supported for normalization by this normalizer.
+     *
+     * @param mixed $data Data to normalize
+     * @param string $format The format being (de-)serialized from or into
+     *
+     * @return bool
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof User;
+    }
+}
