804e373edd28201d7421e14c7e0bf0170f25e7a8
Jeremy Coates <jeremyc@magmadigital.co.uk>
2019-06-04 21:28:33 +0100

    Implement ::getAction
    Added getAction in order to retrieve user information in JSON format.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,16 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 /**
  * Class UserController
@@ -23,5 +30,19 @@ class UserController
     public function createAction(Request $request, EntityManagerInterface $entityManager)
     {
         $data = $request->request;
+    /**
+     * @param string $username
+     * @param UserRepository $userRepository
+     * @Route("/{username}")
+     */
+    public function getAction(string $username, UserRepository $userRepository, SerializerInterface $serializer): Response
+    {
+        $user = $userRepository->getUserByUsername($username);
+
+        if (null !== $user) {
+            return new JsonResponse($serializer->serialize($user, 'json'), 200, [], true);
+        }
+
+        throw new NotFoundHttpException();
     }
 }
